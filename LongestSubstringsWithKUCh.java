cbd301afce6e47af1ff68bdd1cdab86986b0b5ba
Ghulam <109836315+ghulam03@users.noreply.github.com>
2023-08-22 13:45:44 +0530

    Create LongestSubstringsWithKUCh.java

---
+++ LongestSubstringsWithKUCh.java
@@ -0,0 +1,43 @@
+
+
+// User function Template for Java
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        // code here
+        int i=0,j=0;
+        int max=Integer.MIN_VALUE;
+        Map<Character,Integer> map=new HashMap<>();
+        if(k>s.length()){
+            return -1;
+        }
+        while(j<s.length())
+        {
+            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+           // if(map.size()<k)
+            //{
+            //    j++;
+            //}
+            if(map.size()==k)
+            {
+                max=Math.max(max,j-i+1);
+              //  j++;
+            }
+            else if(map.size()>k)
+            {
+                while(map.size()>k)
+                {
+                    map.put(s.charAt(i),map.get(s.charAt(i))-1);
+                         if(map.get(s.charAt(i))==0)
+                         {
+                                map.remove(s.charAt(i));
+                         }
+                    i++;
+                }
+             //   j++;
+            }
+            j++;
+        }
+        return max;
+    }
+}
