846f606ca4af565f6a91642c8a5b09eb84f4965c
Ghulam <109836315+ghulam03@users.noreply.github.com>
2023-08-31 13:43:08 +0530

    lca

---
+++ Solved/Trees/Striver/lca.java
@@ -0,0 +1,23 @@
+//q https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        //result
+        if(left == null) {
+            return right;
+        }
+        else if(right == null) {
+            return left;
+        }
+        else { //both left and right are not null, we found our result
+            return root;
+        }
+    }
+}
