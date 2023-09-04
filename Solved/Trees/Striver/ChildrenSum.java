6792d17c60d242c9029c00799f06302fda30279d
Ghulam <109836315+ghulam03@users.noreply.github.com>
2023-09-04 11:31:16 +0530

    childrenSumm

--- Solved/Trees/Striver/ChildrenSum.java
+++ Solved/Trees/Striver/ChildrenSum.java
@@ -13,6 +13,7 @@ class Node {
 class TUF{
 static void reorder(Node  root) {
   if (root == null) return;
+  //increase root value
   int child = 0;
   if (root . left!=null) {
     child += root . left . data;
@@ -25,14 +26,16 @@ static void reorder(Node  root) {
     if (root . left!=null) root . left . data = root . data;
     else if (root . right!=null) root . right . data = root . data;
   }
+  //
 
   reorder(root . left);
   reorder(root . right);
-
+//making children sum property
   int tot = 0;
   if (root . left!=null) tot += root . left . data;
   if (root . right!=null) tot += root . right . data;
   if (root . left!=null || root . right!=null) root . data = tot;
+  //
 }
 static void changeTree(Node  root) {
   reorder(root);
