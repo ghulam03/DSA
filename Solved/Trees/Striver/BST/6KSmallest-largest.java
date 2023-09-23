5f2c50a29e73c350dd7ade0c97c58cf63cec3a3b
Ghulam <109836315+ghulam03@users.noreply.github.com>
2023-09-23 10:50:27 +0530

    ks

--- Solved/Trees/Striver/BST/6KSmallest-largest.java
+++ Solved/Trees/Striver/BST/6KSmallest-largest.java
@@ -1,1 +1,27 @@
 //q https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+ // Function to find k'th smallest element in BST
+    // Here count denotes the number of nodes processed so far
+
+      static int count = 0;
+    public static Node kthSmallest(Node root, int k)
+    {
+        // base case
+        if (root == null)
+            return null;
+ 
+        // search in left subtree
+        Node left = kthSmallest(root.left, k);
+ 
+        // if k'th smallest is found in left subtree, return it
+        if (left != null)
+            return left;
+ 
+        // if current element is k'th smallest, return it
+        count++;
+        if (count == k)
+            return root;
+ 
+        // else search in right subtree
+        return kthSmallest(root.right, k);
+    }
