8baba3e2f29cf73c1def34a00eb2025ee470718a
Ghulam <109836315+ghulam03@users.noreply.github.com>
2023-10-02 14:54:37 +0530

    lca

--- Solved/Trees/Striver/BST/8lca.java
+++ Solved/Trees/Striver/BST/8lca.java
@@ -1,1 +1,12 @@
+//q https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root.val > Math.max(p.val, q.val))
+      return lowestCommonAncestor(root.left, p, q);
+    if (root.val < Math.min(p.val, q.val))
+      return lowestCommonAncestor(root.right, p, q);
+    return root;
+  }
+}
 
